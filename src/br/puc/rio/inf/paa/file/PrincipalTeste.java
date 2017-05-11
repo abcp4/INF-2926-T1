b270d6a092cf876c7f310646f5009c928b932d7b
flavio-barros <flavio-barros@flavio-barros>
2017-05-11 18:49:38 -0300

    Função de ler todos os arquivos

--- src/br/puc/rio/inf/paa/file/PrincipalTeste.java
+++ src/br/puc/rio/inf/paa/file/PrincipalTeste.java
@@ -1,22 +1,15 @@
 package br.puc.rio.inf.paa.file;
 
-import br.puc.rio.inf.paa.djikstra.array.Edge;
 import br.puc.rio.inf.paa.djikstra.array.GraphInstance;
 
 public class PrincipalTeste {
 
 	public static void main(String[] args) {
 		
-		String file =  "../INF-2926/input/alue2087.stp";
+		ReadAllFiles readAllFiles = new ReadAllFiles();
 		
-		ReadFile readFile = new ReadFile(file);
-		GraphInstance graphInstance = readFile.CreateInstance();
-		
-		for(int key:graphInstance.graph.keySet()){
-			System.out.println("Vertice: "+key);
-			for(Edge edge:graphInstance.graph.get(key)){
-				System.out.println(edge.vertex + " " + edge.distance);
-			}
+		for(GraphInstance graphInstance:readAllFiles.creatAllInstances()){
+			System.out.println(graphInstance.graph.size());
 		}
 		
 	}
