f674682c1892723c8847fc2de166952a5148220e
andersonuchoa <andersonuchoa@andersonuchoa>
2017-05-14 18:31:34 -0300

    melhorias

--- src/br/puc/rio/inf/paa/utils/ReadFile.java
+++ src/br/puc/rio/inf/paa/utils/ReadFile.java
@@ -9,45 +9,44 @@ import java.io.IOException;
 import br.puc.rio.inf.paa.djikstra.GraphInstance;
 
 public class ReadFile {
-	
+
 	private String fileName;
-	
+
 	public ReadFile(String fileName) {
 		this.fileName = fileName;
 	}
-	
-	public GraphInstance createInstance(){
-		
-		Graph graph = null;
-		
-		try (BufferedReader reader = new BufferedReader(new FileReader(new File(fileName)))) {
-		    String line;
-		    while ((line = reader.readLine()) != null) {
-		       String words[] = line.split(" ");
-		       
-		       // Creating graph
-		       if(words[0].equals("Nodes")){
-		    	   int numNodes = Integer.parseInt(words[1]);
-		    	   graph = new Graph(numNodes);
-		       }else if(words[0].equals("E")){
-		    	   int source = Integer.parseInt(words[1]);
-		    	   int destiny = Integer.parseInt(words[2]);
-		    	   int cost = Integer.parseInt(words[3]);
 
-		    	   graph.addEdge(source, destiny, cost);
-		       }
-		       
-		    } 
+	public GraphInstance createInstance() {
+
+		Graph graph = null;
+
+		try (BufferedReader reader = new BufferedReader(new FileReader(new File(fileName)))) {
+			String line;
+			while ((line = reader.readLine()) != null) {
+				String words[] = line.split(" ");
+
+				// Creating graph
+				if (words[0].equals("Nodes")) {
+					int numNodes = Integer.parseInt(words[1]);
+					graph = new Graph(numNodes);
+
+				} else if (words[0].equals("E")) {
+					int source = Integer.parseInt(words[1]);
+					int destiny = Integer.parseInt(words[2]);
+					int cost = Integer.parseInt(words[3]);
+
+					graph.addEdge(source, destiny, cost);
+				}
+
+			}
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return new GraphInstance(graph.getVertices());
-		
+
 	}
-	
+
 }
