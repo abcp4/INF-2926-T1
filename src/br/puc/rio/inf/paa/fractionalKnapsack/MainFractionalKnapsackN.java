d14a6b2d6ca809e52a2409ec1cfefc8c88db6183
anderson-uchoa <andersonguchoa@gmail.com>
2017-06-04 16:18:38 -0300

    this class is a code smells

--- src/br/puc/rio/inf/paa/fractionalKnapsack/MainFractionalKnapsackN.java
+++ src/br/puc/rio/inf/paa/fractionalKnapsack/MainFractionalKnapsackN.java
@@ -9,13 +9,13 @@ import java.util.Map;
 
 import br.puc.rio.inf.paa.fractionalKnapsack.reader.FractionalKnapsackReader;
 import br.puc.rio.inf.paa.utils.CsvWriter;
+import br.puc.rio.inf.paa.utils.Utils;
 
 public class MainFractionalKnapsackN {
 
-
 	public static void main(String[] args) {
 
-		String nameCSV = "nameCSVMainFractionalKnapsackN.csv";
+		String nameCSV = "nameCSVMainFractionalKnapsackNLogN.csv";
 
 		CsvWriter writer = new CsvWriter(nameCSV, ',', Charset.forName("ISO-8859-1"));
 
@@ -36,8 +36,10 @@ public class MainFractionalKnapsackN {
 			writer.write("N");
 			writer.write("CT");
 			writer.write("CPU");
+
 			writer.write("CT/CPU");
 
+			writer.write("Log(CPU)");
 			writer.endRecord();
 
 		} catch (IOException e) {
@@ -51,6 +53,7 @@ public class MainFractionalKnapsackN {
 
 			while (durationEnd <= timeout) {
 				map = knapsackN.knapsack(fractionalKnapsacks.get(i));
+
 				temp_final = System.nanoTime();
 				durationEnd = temp_final - temp_inicio;
 				count++;
@@ -59,16 +62,13 @@ public class MainFractionalKnapsackN {
 			numInstance++;
 			try {
 
-//				ctTime = fractionalKnapsacks.get(i).items.length
-//						* Utils.logBase2(fractionalKnapsacks.get(i).items.length);
-
-
 				ctTime = fractionalKnapsacks.get(i).items.length;
 
-				
 				cpuTime = (durationEnd / count);
 
 				cpuTime = cpuTime / 100;
+
+				double logCPU = Utils.logBase2(cpuTime);
 
 				writer.write(String.valueOf(fractionalKnapsacks.get(i).items.length));
 
@@ -77,6 +77,8 @@ public class MainFractionalKnapsackN {
 				writer.write(new BigDecimal(cpuTime, MathContext.DECIMAL64).toString());
 
 				writer.write(new BigDecimal((ctTime / cpuTime), MathContext.DECIMAL64).toString());
+
+				writer.write(String.valueOf(logCPU));
 
 				writer.endRecord();
 
@@ -112,6 +114,3 @@ public class MainFractionalKnapsackN {
 
 	// }
 }
-
-	
-
